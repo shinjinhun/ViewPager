5ee420718a8c8cc220388e1863592c88dcd713e1
jhshin <mamsorea@gmail.com>
2019-07-05 16:19:23 +0900

    ViewPager

---
+++ app/src/main/java/com/example/viewpager/ThirdFragment.java
@@ -0,0 +1,27 @@
+package com.example.viewpager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class ThirdFragment extends Fragment {
+    public ThirdFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_third, container, false);
+        return  layout;
+    }
+}
