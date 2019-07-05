4446608b7ec6af44ef1b60cad6c558e2c9b01b02
shinjinhun <mamsorea@gmail.com>
2019-07-05 17:51:48 +0900

    ViewPager3

---
+++ viewpagerexample3/src/main/java/com/example/viewpagerexample3/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.viewpagerexample3;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager)findViewById(R.id.vp);
+
+        Button btn_first = (Button)findViewById(R.id.btn_first);
+        Button btn_second = (Button)findViewById(R.id.btn_second);
+        Button btn_third = (Button)findViewById(R.id.btn_third);
+
+        //mViewPager.setAdapter(new pagerAdapter(getSupportFragmentManager()));
+
+        // final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount()); // 오류 발생
+        mViewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        mViewPager.setCurrentItem(0);
+
+        btn_first.setOnClickListener(movePageListener);
+        btn_first.setTag(0);
+        btn_second.setOnClickListener(movePageListener);
+        btn_second.setTag(1);
+        btn_third.setOnClickListener(movePageListener);
+        btn_third.setTag(2);
+    }
+
+    View.OnClickListener movePageListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int tag = (int) v.getTag();
+            mViewPager.setCurrentItem(tag);
+        }
+    };
+
+}
