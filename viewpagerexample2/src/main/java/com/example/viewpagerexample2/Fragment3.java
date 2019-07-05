35c33991c8434a57dc4e4a5aa7ff7537b7323719
shinjinhun <mamsorea@gmail.com>
2019-07-05 17:11:31 +0900

    ViewPager2

---
+++ viewpagerexample2/src/main/java/com/example/viewpagerexample2/Fragment3.java
@@ -0,0 +1,23 @@
+package com.example.viewpagerexample2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends Fragment {
+    public Fragment3(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // return super.onCreateView(inflater, container, savedInstanceState);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+        return rootView;
+    }
+}
